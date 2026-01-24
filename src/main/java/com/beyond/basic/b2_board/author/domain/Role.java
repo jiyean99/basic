a9585fa19dd0e44f53868957c7ee9282e0ad26bd
이지연 <wldus019113@icloud.com>
2026-01-25 01:16:33 +0900

    - Role 열거형 자료 추가

--- src/main/java/com/beyond/basic/b2_board/author/domain/Role.java
+++ src/main/java/com/beyond/basic/b2_board/author/domain/Role.java
@@ -7,6 +7,7 @@ package com.beyond.basic.b2_board.author.domain;
  * - 내부적으로 숫자값(순서대로 0, 1...)을 가짐
  * - @Enumerated(EnumType.STRING)으로 DB에 문자열로 저장 (Author.java 참조)
  * - 기본 생성자/메서드 불가, 순서대로 ADMIN(0) < USER(1)
+ *
  */
 public enum Role {
     ADMIN, USER
