1e7ae796ba30289ea9482483766deda28c42a3b9
이지연 <wldus019113@icloud.com>
2026-01-25 01:08:19 +0900

    - Role 열거형 자료 추가

--- src/main/java/com/beyond/basic/b2_board/author/domain/Role.java
+++ src/main/java/com/beyond/basic/b2_board/author/domain/Role.java
@@ -1,6 +1,13 @@
 package com.beyond.basic.b2_board.author.domain;
 
+/**
+ * Author 권한 레벨을 정의하는 Enum
+ *
+ * [Enum 특징]
+ * - 내부적으로 숫자값(순서대로 0, 1...)을 가짐
+ * - @Enumerated(EnumType.STRING)으로 DB에 문자열로 저장 (Author.java 참조)
+ * - 기본 생성자/메서드 불가, 순서대로 ADMIN(0) < USER(1)
+ */
 public enum Role {
-    // enum은 내부적으로 숫자값을 가지고있다.
     ADMIN, USER
 }
